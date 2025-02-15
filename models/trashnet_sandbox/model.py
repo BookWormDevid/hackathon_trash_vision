af38a7d8e536fd9937d45b9f726a4e46f6951ddc
BookWormDevid <94227014+BookWormDevid@users.noreply.github.com>
2025-02-15 13:37:23 +0300

    Add files via upload

--- models/trashnet_sandbox/model.py
+++ models/trashnet_sandbox/model.py
@@ -38,18 +38,23 @@ data_augmentation = tf.keras.Sequential([
 base_model = tf.keras.applications.MobileNetV2(
     input_shape=(224, 224, 3), include_top=False, weights="imagenet"
 )
-base_model.trainable = True
+base_model.trainable = False
+#for layer in base_model.layers[:-60]:
+    #layer.trainable = False
 
 model = tf.keras.Sequential([
     data_augmentation,
     tf.keras.layers.Rescaling(1./255),
     base_model,
     tf.keras.layers.GlobalAveragePooling2D(),
-    tf.keras.layers.Dense(64, activation='elu'),
-    tf.keras.layers.Dense(32, activation='elu'),
+    tf.keras.layers.Dense(4096, activation='elu'),
+    tf.keras.layers.Dense(1024, activation='elu'),
+    tf.keras.layers.Dense(128, activation='elu'),
     tf.keras.layers.Dropout(0.4),
     tf.keras.layers.Dense(len(class_names), activation='softmax')
 ])
+model.build()
+
 
 model.compile(
     optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
@@ -58,7 +63,7 @@ model.compile(
 )
 model.summary()
 
-history = model.fit(train_ds, validation_data=val_ds, epochs=10)
+history = model.fit(train_ds, validation_data=val_ds, epochs=4)
 
 # Построение графиков точности и потерь
 acc = history.history['accuracy']
