c6e702025ab144349041e32f213061bbd9ebfae3
BookWormDevid <94227014+BookWormDevid@users.noreply.github.com>
2025-02-16 11:58:03 +0300

    Add files via upload

--- models/trashnet_sandbox/BITA.py
+++ models/trashnet_sandbox/BITA.py
@@ -16,12 +16,13 @@ def bita(big_image_path, t_num):
         tile_info = image_eval.choose_and_predict(complete_path)
         tiles_info.append(tile_info)
         print(i)
+        if tile_info[0] != 'cardboard':
+            print(str(tile_info[0]) + " at" + str(tile_info[1]) + "%")
         i += 1
 
     return tiles_info
 
 
 tiles = 64
-pred = bita('data/Possible_trash_on_platforms/platform_10.jpg', tiles)
+pred = bita('data/Possible_trash_on_platforms/platform_16.jpg', tiles)
 
-print(pred)
